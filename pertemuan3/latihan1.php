1c37d34264fb27ffb37743243ab69304abcf56dc
DESKTOP-EJTQDQ8\Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2022-04-06 14:01:16 +0700

    pertemuan 3 PHP Dasar

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,30 @@
+<?php
+
+    // pengulangan
+    // for
+    for($i = 0; $i < 5; $i++ ){
+        echo "Hello, World! <br>";
+    }
+
+    for($i = 8; $i > 5; $i-- ){
+        echo "ini adalah decrement";
+    }
+
+    // while
+    $i = 0;
+    while($i < 8){
+        echo "Hello, World <br>";
+
+    $i++;
+    }
+
+    // do.. while
+    $i = 0;
+    do {
+        echo " ini do while<br>";
+        $i++; 
+    } while($i<8)
+
+    //foreach : pengulangan khusus array
+
+?>
---
+++ pertemuan3/latihan3.php
@@ -0,0 +1,21 @@
+<?php 
+
+    // percabangan
+    // if else
+    // if else if else
+    // ternary
+    // switch
+
+    // sintaks if
+    $x = 30;
+    if($x < 20){
+        echo "benar";
+    } else if($x == 30){
+        echo "yuhuhu";
+    } else{
+        echo "salah";
+    }
+
+    // sintaks if else if else
+
+?>
